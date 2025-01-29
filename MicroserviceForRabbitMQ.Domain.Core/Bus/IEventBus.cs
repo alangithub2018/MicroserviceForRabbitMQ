7c8b1b61939e4094710ac2c7d3bd8dbc06ec5541
Eric Alan Rodriguez Contreras <licalan.sistemas@gmail.com>
2025-01-29 13:48:46 -0600

    implemented interfaces and classes for Domain.Core

---
+++ MicroserviceForRabbitMQ.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,21 @@
+﻿using MicroserviceForRabbitMQ.Domain.Core.Commands;
+using MicroserviceForRabbitMQ.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroserviceForRabbitMQ.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+
+        void Publish<T>(T @event) where T : Event;
+
+        void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>;
+    }
+}

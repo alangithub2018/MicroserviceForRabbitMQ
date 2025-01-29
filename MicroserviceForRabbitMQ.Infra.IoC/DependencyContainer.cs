ceedccae1364a3d04ef84c3ed30e463dc0d57468
Eric Alan Rodriguez Contreras <licalan.sistemas@gmail.com>
2025-01-29 14:37:20 -0600

    Initial commit of Infrastructure Dependency project

---
+++ MicroserviceForRabbitMQ.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,20 @@
+﻿using MicroserviceForRabbitMQ.Domain.Core.Bus;
+using MicroserviceForRabbitMQ.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroserviceForRabbitMQ.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
